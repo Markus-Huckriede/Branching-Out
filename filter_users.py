9acbff79b266635f7034cbec135727eaf3ec3253
Markus Huckriede <markushuckriede@MacBook-Air-von-Markus.fritz.box>
2025-06-23 12:29:24 +0200

    function filter_by_email added

--- filter_users.py
+++ filter_users.py
@@ -11,11 +11,24 @@ def filter_users_by_name(name):
         print(user)
 
 
+def filter_by_email(email):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users =[user for user in users if user.get("email", "").lower() == email.lower()]
+
+    for user in filtered_users:
+        print(user)
+
+
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (name, age or email): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+    elif filter_option == "email":
+        email_to_search = input("Enter an email to filter users: ").strip()
+        filter_users_by_email(email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
